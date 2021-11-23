b052007357ac7d7da2dc9dbd5c4a754c05f28fc0
tdoval <thiagodovalaraujo@gmail.com>
2021-11-23 19:33:34 -0300

    evento de mostrar o presente

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,13 +1,25 @@
 import { useState } from "react";
 import Porta from "../components/Porta";
 import Presente from "../components/Presente";
+import { atualizarPortas, criarPortas } from "../functions/portas"
 import PortaModel from "../model/Porta";
 
 export default function Home() {
-  const [p1, setP1] = useState(new PortaModel(1))
+  const [portas, setPortas] = useState(criarPortas(3,2))
+
+  function renderizarPortas() {
+    return portas.map(porta => {
+      return <Porta key={porta.numero} value={porta}
+      onChange={novaPorta => {
+        setPortas(atualizarPortas(portas, novaPorta))
+      }}
+      />
+    })
+  }
+
   return (
     <div style={{display:"flex"}}>
-      <Porta value={p1} onChange={novaPorta => setP1(novaPorta)}/>
+      {renderizarPortas()}
     </div>
   )
 }
