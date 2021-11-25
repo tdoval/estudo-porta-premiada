84a2ca63ba8003179f8f4d2719f52747f310fa4a
tdoval <thiagodovalaraujo@gmail.com>
2021-11-25 15:07:32 -0300

    router

---
+++ pages/jogo/[portas]/[temPresente].tsx
@@ -0,0 +1,43 @@
+import styles from "../../../styles/jogo.module.css"
+import { useState, useEffect } from "react";
+import Porta from "../../../components/Porta";
+import Presente from "../../../components/Presente";
+import { atualizarPortas, criarPortas } from "../../../functions/portas"
+import Link from "next/link"
+import { useRouter } from "next/router"
+
+export default function jogo() {
+  const router = useRouter()
+
+    const [portas, setPortas] = useState([])
+
+    useEffect(() => {
+      const portas = +router.query.portas
+      const temPresente = +router.query.temPresente
+      setPortas(criarPortas(portas, temPresente))
+    }, [router?.query])
+    
+    
+
+    function renderizarPortas() {
+      return portas.map(porta => {
+        return <Porta key={porta.numero} value={porta}
+        onChange={novaPorta => {
+          setPortas(atualizarPortas(portas, novaPorta))
+        }}
+        />
+      })
+    }
+    return (
+        <div id={styles.jogo}>
+            <div className={styles.portas}>
+            {renderizarPortas()}
+            </div>
+            <div className={styles.botoes}>
+                <Link href="/">
+                    <button>Reiniciar jogo</button>
+                </Link>
+            </div>
+      </div>
+    )
+}
